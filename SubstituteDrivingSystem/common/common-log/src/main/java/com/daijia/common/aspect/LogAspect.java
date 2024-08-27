a2a627c6d62823bbc8f28b6c00764589edfc903b
glowworm <15159885771@163.com>
2024-08-27 18:27:56 +0800

    工具类编写

--- SubstituteDrivingSystem/common/common-log/src/main/java/com/daijia/common/aspect/LogAspect.java
+++ SubstituteDrivingSystem/common/common-log/src/main/java/com/daijia/common/aspect/LogAspect.java
@@ -2,11 +2,17 @@ package com.daijia.common.aspect;
 
 import com.daijia.common.annoation.Log;
 import jakarta.annotation.Resource;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.SneakyThrows;
 import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.AfterReturning;
 import org.aspectj.lang.annotation.Aspect;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
+import org.springframework.web.context.request.RequestAttributes;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
 
 /**
  * Description： TODO
@@ -23,7 +29,34 @@ public class LogAspect {
     @Resource
     private SysOperaLogFeignClient sysOperaLogFeignClient;
 
+    /***
+     * 完成请求后执行
+     * @param joinPoint
+     * @param controllerLog
+     * @param jsonResult
+     */
+    @AfterReturning(pointcut = "@annotation(controllerLog)",returning = "jsonResult")
     public void doAfterReturning(JoinPoint joinPoint, Log controllerLog, Object jsonResult){
+        handleLog(joinPoint, controllerLog, null, jsonResult);
+        
+    }
+
+    /**
+     * 拦截异常操作
+     * @param joinPoint
+     * @param controllerLog
+     * @param o
+     * @param jsonResult
+     */
+    @SneakyThrows
+    protected void handleLog(final JoinPoint joinPoint, Log controllerLog, Object o, Object jsonResult) {
+
+        RequestAttributes ra = RequestContextHolder.getRequestAttributes();
+        ServletRequestAttributes  sra =(ServletRequestAttributes) ra;
+        HttpServletRequest request = sra.getRequest();
+
+        // *======数据库日志======//
+        new SysOperLog();
 
     }
 
