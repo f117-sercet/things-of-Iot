02a0161dc5250c62b98981383a215f9f77c133af
glowworm <15159885771@163.com>
2025-03-07 17:16:51 +0800

    service-rules 模块编写

--- SubstituteDrivingSystem/service/service-rules/src/main/java/com/atguigu/daijia/rules/service/impl/RewardRuleServiceImpl.java
+++ SubstituteDrivingSystem/service/service-rules/src/main/java/com/atguigu/daijia/rules/service/impl/RewardRuleServiceImpl.java
@@ -1,9 +1,13 @@
 package com.atguigu.daijia.rules.service.impl;
 
+import com.atguigu.daijia.model.form.rules.RewardRuleRequest;
 import com.atguigu.daijia.model.form.rules.RewardRuleRequestForm;
+import com.atguigu.daijia.model.vo.rules.RewardRuleResponse;
 import com.atguigu.daijia.model.vo.rules.RewardRuleResponseVo;
 import com.atguigu.daijia.rules.service.RewardRuleService;
+import com.atguigu.daijia.rules.utils.DroolsHelper;
 import lombok.extern.slf4j.Slf4j;
+import org.kie.api.runtime.KieSession;
 import org.springframework.stereotype.Service;
 
 /**
@@ -20,6 +24,30 @@ public class RewardRuleServiceImpl implements RewardRuleService {
 
     @Override
     public RewardRuleResponseVo calculateOrderRewardFee(RewardRuleRequestForm rewardRuleRequestForm) {
-        return null;
+        //封装传入参数对象
+        RewardRuleRequest rewardRuleRequest = new RewardRuleRequest();
+        rewardRuleRequest.setOrderNum(rewardRuleRequestForm.getOrderNum());
+
+        //创建规则引擎对象
+        KieSession kieSession = DroolsHelper.loadForRule(RULES_CUSTOMER_RULES_DRL);
+
+        //封装返回对象
+        RewardRuleResponse rewardRuleResponse = new RewardRuleResponse();
+        kieSession.setGlobal("rewardRuleResponse",rewardRuleResponse);
+
+        //设置对象，触发规则
+        kieSession.insert(rewardRuleRequest);
+        kieSession.fireAllRules();
+
+        //终止会话
+        kieSession.dispose();
+
+        //封装RewardRuleResponseVo
+        RewardRuleResponseVo rewardRuleResponseVo = new RewardRuleResponseVo();
+        rewardRuleResponseVo.setRewardAmount(rewardRuleResponse.getRewardAmount());
+        return rewardRuleResponseVo;
+
+
+
     }
 }
