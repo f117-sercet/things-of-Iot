12c6e3330c238b5ba475ec7ea255f966a302028a
glowworm <15159885771@163.com>
2025-03-10 15:41:46 +0800

    service-system模块编写

--- SubstituteDrivingSystem/service/service-system/src/main/java/com/atguigu/daijia/ServiceSystemApplication.java
+++ SubstituteDrivingSystem/service/service-system/src/main/java/com/atguigu/daijia/ServiceSystemApplication.java
@@ -1,23 +1,17 @@
-package com.atguigu.daijia.rules;
+package com.atguigu.daijia;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
-/**
- * Description： TODO
- *
- * @author: 段世超
- * @aate: Created in 2025/2/19 10:17
- */
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableFeignClients
-// 项目启动报错修改
-public class ServiceRulesApplication {
+public class ServiceSystemApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(ServiceRulesApplication.class,args);
+        SpringApplication.run(ServiceSystemApplication.class, args);
     }
+
 }
